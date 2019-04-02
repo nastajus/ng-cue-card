0fb8817f42814ab78ef9a61df190ca87e826438a
Ian Nastajus <ian.nastajus@gmail.com>
2019-04-02 00:32:49 -0400

    animations on cards now, pretty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CueCardComponent } from './components/cue-card/cue-card.component';
 import { CueCardListComponent } from './components/cue-card-list/cue-card-list.component';
 import { DbFakeComponent } from './components/db-fake/db-fake.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { DbFakeComponent } from './components/db-fake/db-fake.component';
     DbFakeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [DbFakeComponent],
   bootstrap: [AppComponent]
--- src/app/components/cue-card/cue-card.component.ts
+++ src/app/components/cue-card/cue-card.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CueCard } from 'src/app/models/cue-card';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-cue-card',
   templateUrl: './cue-card.component.html',
-  styleUrls: ['./cue-card.component.css']
+  styleUrls: ['./cue-card.component.scss'],
+  // animations: CueCardAnimations
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateY(179.9deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in'))
+    ])  
+  ]
 })
 export class CueCardComponent implements OnInit {
 
   @Input() cueCard: CueCard
 
-  constructor() { }
+  flip: string = 'inactive';
+  constructor() {}
+
+  toggleFlip() {
+    this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
+  }
+
 
   ngOnInit() {
   }
